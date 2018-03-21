56ff704ffa48a25fff7619e73540ae6301a8a8cb
Kory-McLean <krock0606@candw.ky>
2018-03-21 17:30:30 -0400

    Completion of diag_1

--- game_board.v
+++ game_board.v
@@ -98,13 +98,13 @@ module game_board(Clk, reset, out);
 								 board[42], board[43], board[58], board[59], board[74], board[75], board[90], board[91]};
 								 
 	// TODO: complete diagonal_1: top right to bottom left direction
-	wire [85:0] diagonal_1 = {board[6], board[7], board[22], board[23], board[38], board[39], board[54], board[55], 2'b00,
-								 board[4], board[5], board[20], board[21], board[36], board[37], board[52], board[53], board[68], board[69], 2'b00,
-								 board[2], board[3], board[18], board[19], board[34], board[35], board[50], board[51], board[66], board[67], board[82], board[83], 2'b00,
-								 board[0], board[1], board[16], board[17], board[32], board[33], board[48], board[49], board[64], board[65], board[80], board[81], board[96], board[97], 2'b00,
-								 board[14], board[15], board[30], board[31], board[46], board[47], board[62], board[63], board[78], board[79], board[94], board[95], 2'b00,
-								 board[28], board[29], board[44], board[45], board[60], board[61], board[76], board[77], board[92], board[93], 2'b00,
-								 board[42], board[43], board[58], board[59], board[74], board[75], board[90], board[91]};
+	wire [85:0] diagonal_1 = {board[42], board[43], board[30], board[31], board[18], board[19], board[6], board[7], 2'b00,
+								 board[56], board[57], board[44], board[45], board[32], board[33], board[20], board[21], board[8], board[9], 2'b00,
+								 board[70], board[71], board[58], board[59], board[46], board[47], board[34], board[35], board[22], board[23], board[10], board[11], 2'b00,
+								 board[84], board[85], board[72], board[73], board[60], board[61], board[48], board[49], board[36], board[37], board[24], board[25], board[12], board[13], 2'b00,
+								 board[86], board[87], board[74], board[75], board[62], board[63], board[50], board[51], board[38], board[39], board[26], board[27], 2'b00,
+								 board[88], board[89], board[76], board[77], board[64], board[65], board[52], board[53], board[40], board[41], 2'b00,
+								 board[90], board[91], board[78], board[79], board[66], board[67], board[54], board[55]};
 								 
 								 
 	// Combine all rows, columns, and diagonals with padding into one large string holding all possible winning combinations
